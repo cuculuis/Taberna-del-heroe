4f15270ef7b8db8a4d16d08739f110d7befa7eae
Luis Angel Lopez Solis <luisangel96@gmail.com>
2022-06-29 23:59:40 -0300

    Commit 2 MapyPromises: Modificación de navbar

---
+++ src/components/Carrito.js
@@ -0,0 +1,7 @@
+import cart from '../assets/cart.png';
+
+export default function Carrito(){
+    return(
+        <img src={cart} alt="Carrito" className='img-Nav'/>
+    )
+    }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
-import loguito from "../assets/loguito.jpg";
+import loguito from '../assets/loguito.jpg';
 import 'bootstrap/dist/css/bootstrap.css';
 import styles from "./NavBar.css";
+import Carrito from './Carrito';
 
 
 
@@ -8,12 +9,14 @@ import styles from "./NavBar.css";
 export default function NavBar(){
     return(
         <nav className="navMain">
-            <img src="../assets/loguito.jpg" alt="logo" />
+            <img src={ loguito } alt="logo" className='img-Nav'/>
             <ul className="navList">
                 <a className="buttom-primary"><li className="title">Productos</li></a>
                 <a className="buttom-primary"><li className="title">Servicios</li></a>
                 <a className="buttom-primary"><li className="title">Contacto</li></a> 
             </ul>
+
+        <Carrito />
         </nav>
     );
 }
