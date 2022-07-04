8a172eee30316f751991b417a40465574a99410e
Luis Angel Lopez Solis Remoto <luisangel96@gmail.com>
2022-07-04 03:12:18 -0300

    Commit 2 ButtomCount: Añadir configuración de los botones

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,18 @@
+import Contador from "./ItemCount";
+
 
 function Card({product, price}) {
     return (
-        <div>
-            <h3>
-                {product}
-            </h3>
-            <h4>
-                {price}
-            </h4>    
+        <div className="container">
+            <div className="card col-md-3">
+                <h3>
+                    {product}
+                </h3>
+                <h4>
+                    {price}
+                </h4>  
+                <Contador />
+            </div>
         </div>
 )}
 
