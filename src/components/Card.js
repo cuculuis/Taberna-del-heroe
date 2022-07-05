d8b36509c4663b292dcb564b4b7cb936721e7559
Luis Angel Lopez Solis <luisangel96@gmail.com>
2022-07-04 22:15:37 -0300

    Commit 3: Modificación de las cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,14 @@ import Contador from "./ItemCount";
 function Card({product, price}) {
     return (
         <div className="container">
-            <div className="card col-md-3">
+            <div className="card-body col-md-3">
                 <h3>
                     {product}
                 </h3>
                 <h4>
                     {price}
                 </h4>  
-                <Contador />
+                {/* <Contador /> */}
             </div>
         </div>
 )}
--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,17 +1,20 @@
-function Contador() {
-    return (
-        const { count, decrement, increment } = useContext(Context)
-        <div>
-            <h2>
-                Contador
-            </h2>
-        </div>
-        <div>
-            <Button ClassName="primary data-testid button_minus" onClick={decrement}>-</Button>
-            <Label ClassName="data-testid contador" circular color={color(count)} size="huge">{count}</Label>
-            <Button secondary data-testid="button_plus" onClick={increment}>+</Button>
-        </div>
-    )
-}
+// import React, { Component, useState } from "react";
 
-export default Contador
+
+// function Contador() {
+//     return (
+//         const { count, decrement, increment } = useContext(Context)
+//         <div>
+//             <h2>
+//                 Contador
+//             </h2>
+//         </div>
+//         <div>
+//             <Button ClassName="primary data-testid button_minus" onClick={decrement}>-</Button>
+//             <Label ClassName="data-testid contador" circular color={color(count)} size="huge">{count}</Label>
+//             <Button secondary data-testid="button_plus" onClick={increment}>+</Button>
+//         </div>
+//     )
+// }
+
+// export default Contador
